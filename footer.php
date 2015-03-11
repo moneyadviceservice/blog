ce754c21d442728bac781e1025a80fc8f8ceb477
alexwllms <alex.williams@moneyadviceservice.org.uk>
2015-03-11 21:25:20 +0000

    Adjustments to footer styles and normalise font sizes

--- footer.php
+++ footer.php
@@ -10,6 +10,15 @@
 
 	</div><!-- #content -->
 
+  <section class="l-about-us">
+    <div class="l-constrained">
+      <div class="l-about-us__content">
+        <h2>About the MAS Dev Team</h2>
+        <p class="l-about-us__content">Welcome to the blog from the digital and development teams at the Money Advice Service. We write about things that we hope will be of interest to anyone who’s involved in making digital products. Whether you care about user research, UX and design, front-end development, backend development, testing, and everything in-between, there should be stories here for you. We write about the lessons we’ve learnt, the things that went well, the problems we’ve overcome, as well as those things we’re still figuring out.</p>
+      </div>
+    </div>
+  </section>
+
 	<footer class="l-footer" role="contentinfo">
     <div class="l-constrained">
       <p class="l-footer__content">Making Money Advice was created by the development team at the <a href="https://www.moneyadviceservice.org.uk">Money Advice Service</a>.</p>
